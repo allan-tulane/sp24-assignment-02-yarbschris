c695ee6060d27f3e7901858fe05105a7cd8dc4b1
yarbschris <123770805+yarbschris@users.noreply.github.com>
2024-03-03 23:11:17 -0600

    Added tests

--- test_main.py
+++ test_main.py
@@ -3,3 +3,7 @@ from main.py import *
 ## Feel free to add your own tests here.
 def test_multiply():
     assert subquadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+    assert subquadratic_multiply(BinaryNumber(0), BinaryNumber(98531)) == 0
+    assert subquadratic_multiply(BinaryNumber(493293), BinaryNumber(0)) == 0
+    assert subquadratic_multiply(BinaryNumber(3), BinaryNumber(4)) == 3*4
+    assert subquadratic_multiply(BinaryNumber(1), BinaryNumber(16)) == 1*16
